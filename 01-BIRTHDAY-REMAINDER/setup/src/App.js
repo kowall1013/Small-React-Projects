64a2342bbb2d41935b92ecc56d457ec8cafa86ba
Mateusz Kowalski <mkmotywacja@gmail.com>
2021-04-15 09:18:04 +0200

    feat:Done Project Birthday

--- 01-BIRTHDAY-REMAINDER/setup/src/App.js
+++ 01-BIRTHDAY-REMAINDER/setup/src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  function handleClick() {
+    setPeople([]);
+  }
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={handleClick}>Clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
