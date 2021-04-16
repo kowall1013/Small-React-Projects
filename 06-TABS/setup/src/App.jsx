466293cc4f2c74ac59de968549fbdb0ffa15fa9d
Mateusz Kowalski <mkmotywacja@gmail.com>
2021-04-16 08:35:17 +0200

    feat:5th project done

--- 06-TABS/setup/src/App.jsx
+++ 06-TABS/setup/src/App.jsx
@@ -1,52 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project";
+
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [jobs, setJobs] = useState([])
-  const [value, setValue] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
 
   const fetchJobs = async () => {
-    const reponse = await fetch(url)
-    const newJobs = await reponse.json()
-    setJobs(newJobs)
-    setLoading(false)
-  }
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
   useEffect(() => {
-    fetchJobs()
-  }, [])
+    fetchJobs();
+  }, []);
   if (loading) {
     return (
       <section className="section loading">
-        <h1>Loading...</h1>
+        <h1>loading...</h1>
       </section>
-    )
+    );
   }
-  const { company, dates, duties, title } = jobs[value]
+  const { company, dates, duties, title } = jobs[value];
   return (
     <section className="section">
       <div className="title">
-        <h2>experience</h2>
+        <h2>expirience</h2>
         <div className="underline"></div>
       </div>
       <div className="jobs-center">
-        {/* btn container */}
         <div className="btn-container">
           {jobs.map((item, index) => {
             return (
               <button
-                key={item.id}
+                key={index}
                 onClick={() => setValue(index)}
-                className={`job-btn ${index === value && 'active-btn'}`}
+                className={`job-btn ${index === value && "active-btn"}`}
               >
                 {item.company}
               </button>
-            )
+            );
           })}
         </div>
-        {/* job info */}
         <article className="job-info">
           <h3>{title}</h3>
           <h4>{company}</h4>
@@ -57,15 +56,12 @@ function App() {
                 <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
                 <p>{duty}</p>
               </div>
-            )
+            );
           })}
         </article>
       </div>
-      <button type="button" className="btn">
-        more info
-      </button>
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
